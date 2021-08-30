b326779de38701e1091841d3b5194018b402c2fd
tleal1 <sr.leal__@hotmail.com>
2021-08-30 14:37:19 -0300

    first commit

---
+++ src/componentes/button.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './button.css'
+
+export default (props) => {
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
+    return (
+        <button
+        onClick={(e) => props.click && props.click(props.label)}
+        className={classes}
+        >
+            {props.label}
+        </button>
+    );
+};
